9c3a7bc1b51e50bb6c37c657f25c5fe698e24b23
jnegrete63 <jnegrete@uwyo.edu>
2024-11-20 03:06:22 +0000

    compleated asignment

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -1,22 +1,55 @@
-# Your Name Here
+# Jose Fanian Negrete osegura
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Submission Date: 11/19/2024
+# Lab 10
+# Lab Section:11
+# Sources, people worked with, help given to: help from TA'S and help from co-pilot
 # your
 # comments
 # here
 
 #import modules you will need 
 
-from hashlib import sha256 
-from pathlib import Path
 
-def get_hash(to_hash):
-    """You can use """
-    return sha256(to_hash.encode('utf-8')).hexdigest().upper()
 
+def uncover_password():
+    from hashlib import sha256 
+    def get_hash(to_hash):
+        """You can use this to hash the passwords from the file of forbiden paswprds."""
+        return sha256(to_hash.encode('utf-8')).hexdigest().upper()
+    try:
+        with open('hash', 'r') as hash:
+            hash_1 = hash.read().strip()
+    except FileNotFoundError:
+        print("The hash file you are lookign for is not here, thats lame, try again.")
+        return
+    except Exception as braking_through:
+        print(f"Boooooo, there seems to have been an error looking for the items, try to hack again if you may: {braking_through}")
+        return
+
+    try:
+        from pathlib import Path
+        path = Path ('rockyou.txt')
+        contents = path.read_text()
+        lists = contents.splitlines()
+    except FileNotFoundError:
+        print("sorry there were no passwords found here, try again.")
+        return
+    except Exception as braking_through:
+        print(f"Sommething went wrong while haking the password, try again: {braking_through}")
+        return
+    else:
+        found_password = ""
+        for password in lists:
+            password = password.strip()
+            if get_hash(password) == hash_1:
+                found_password = password
+                
+                break
+        if found_password:
+            print(f"Great Haking!!!, you found the password, it is: {password}")
+
+uncover_password()
 
 
 # Files and Exceptions
